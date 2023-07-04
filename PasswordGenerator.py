f49043dee0e3d32e8ba756e627c9e64da1941abd
mariusmot <marius.mot79@gmail.com>
2023-07-04 08:46:34 +0300

    4'th of July commit

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -6,7 +6,7 @@ numbers = '0123456789'
 symbols = "[]{}()*;/,._-"
 
 all = lower + upper + numbers + symbols
-length = 16
+length = 15
 password = "".join(random.sample(all,length))
 
 print(password)
